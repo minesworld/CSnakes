be7c933191f3ba1404a20039159871fc0cb3acbc
CaptainWelpe <michael.keller@minesworld.de>
2024-10-31 09:41:56 +0100

    - added PythonObject.Steal to create a PythonObject from a pyoPtr - added Python.CreateAndImportModuleFromSource() function

--- src/CSnakes.Runtime/Python/Import.cs
+++ src/CSnakes.Runtime/Python/Import.cs
@@ -8,4 +8,15 @@ public static class Import
     {
         return PythonObject.Create(CAPI.Import(module));
     }
+
+    public static PythonObject CreateAndImportModuleFromSource(string name, string source, string filename = "<string>")
+    {
+        PythonObject result;
+        using (GIL.Acquire())
+        {
+            result = PythonObject.Steal(CAPI.AddCodeAsModule(source, name, filename));
+        }
+
+        return result;
+    }
 }
