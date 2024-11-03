e3f8589f7b720cd983c51aef0b1fa5d7eedbc163
CaptainWelpe <michael.keller@minesworld.de>
2024-11-03 17:18:27 +0100

    - prepared file i/o for sourcegenerator tool

--- src/sourcegenerator/Generator.cs
+++ src/sourcegenerator/Generator.cs
@@ -3,36 +3,75 @@ using CSnakes.Parser;
 using CSnakes.Reflection;
 using CSnakes;
 using Microsoft.CodeAnalysis.Text;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace sourcegenerator;
 internal class Generator
 {
+    static public bool WriteCSharpFileFromPyFile(
+        string @namespace,
+        string pyPath,
+        Encoding pyFileEncoding,
+        string csDirectoryPath,
+        string csExtension,
+        out string csPath,
+        Encoding csFileEncoding,
+        out GeneratorError[]? errors,
+        bool dryRun = false)
+    {
+        csPath = String.Empty;
+
+        string csFileNameWithoutExtension;
+        string csSource;
+
+        if (CSharpStringFromPyFile(@namespace, pyPath, pyFileEncoding, out csFileNameWithoutExtension, out csSource, out errors) == false) return false;
+
+        // calculate and write file to csPath
+        csPath = Path.Join(csDirectoryPath, csFileNameWithoutExtension + csExtension);
+        if (dryRun == false) File.WriteAllText(csPath, csSource, csFileEncoding);
+
+        return true;
+    }
+
+
+    static public bool CSharpStringFromPyFile(
+        string @namespace,
+        string pyPath,
+        Encoding pyFileEncoding,
+        out string csFileNameWithoutExtension,
+        out string csSource,
+        out GeneratorError[]? errors)
+    {
+        // read file from pyPath
+        var pySource = File.ReadAllText(pyPath, pyFileEncoding);
+
+        return Generate(@namespace, pyPath, pySource, out csFileNameWithoutExtension, out csSource, out errors);
+    }
+
+
     static public bool Generate(
         string @namespace,
         string pyPath,
-        SourceText pyCode,
-        out string csFileName,
-        out string? csCode,
+        string pySource,
+        out string csFileNameWithoutExtension,
+        out string csSource,
         out GeneratorError[]? errors)
     {
+        csSource = String.Empty;
+        errors = null;
+
         var fileName = Path.GetFileNameWithoutExtension(pyPath);
 
         // Convert snakecase to pascal case
-        var pascalFileName = string.Join("", fileName.Split('_').Select(s => char.ToUpperInvariant(s[0]) + s.Substring(1)));
-        csFileName = $"{pascalFileName}.py.cs";
+        csFileNameWithoutExtension = string.Join("", fileName.Split('_').Select(s => char.ToUpperInvariant(s[0]) + s.Substring(1)));
 
         // Parse the Python file
-        var result = PythonParser.TryParseFunctionDefinitions(pyCode, out PythonFunctionDefinition[] functions, out errors);
+        var result = PythonParser.TryParseFunctionDefinitions(SourceText.From(pySource), out PythonFunctionDefinition[] functions, out errors);
 
         if (result)
         {
             IEnumerable<MethodDefinition> methods = ModuleReflection.MethodsFromFunctionDefinitions(functions, fileName);
-            csCode = PythonStaticGenerator.FormatClassFromMethods(@namespace, pascalFileName, methods, fileName, functions);
+            csSource = PythonStaticGenerator.FormatClassFromMethods(@namespace, csFileNameWithoutExtension, methods, fileName, functions);
 
             /*
             sourceContext.AddSource($"{pascalFileName}.py.cs", source);
@@ -51,7 +90,6 @@ internal class Generator
             */
         }
 
-        csCode = null;
         return false;
     }
 }
--- src/sourcegenerator/Program.cs
+++ src/sourcegenerator/Program.cs
@@ -1,18 +1,12 @@
 ﻿using CSnakes;
-using CSnakes.Parser;
-using CSnakes.Parser.Types;
-using CSnakes.Reflection;
 using DocoptNet;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Text;
 using sourcegenerator;
-using System.IO;
 using System.Text;
 
 const string usage = @"sourcegenerator
 
 Usage:
-  sourcegenerator.exe [--namespace=<n>] [-R] [-B=<basedir>] <pysource>...
+  sourcegenerator.exe [--namespace=<n>] [--dry] [--csext=<ext>] [-R] [-B=<basedir>] <pysource>...
   sourcegenerator.exe [--namespace=<n>] -O <pysource>
   sourcegenerator.exe (-h | --help)
   sourcegenerator.exe --version
@@ -21,56 +15,62 @@ Options:
   -h --help        Show this screen.
   --version        Show version.
   --namespace=<n>  C# namespace for generated class [default: CSnakes.Service]
-  -R               Recursive 
+  --csext=<ext>    Use C# file extension [default: .py.cs] 
+  -R               Recurse into directories.
   -B=<basebdir>    Write generated .cs files starting at basedir [default: .]
-  -O               Write generated .cs file to stdout
+  --dry            Dry run, does not write generated .cs files.
+  -O               Write generated .cs file to stdout.
 ";
 
 var arguments = new Docopt().Apply(usage, args, version: "sourcegenerator 0.1", exit: true)!;
-foreach (var (key, value) in arguments)
-    Console.WriteLine("{0} = {1}", key, value);
-
 
 var @namespace = arguments["--namespace"].ToString();
+var csExtension = arguments["--csext"].ToString();
 var useStdout = arguments["-O"].IsTrue;
+var recursive = arguments["-R"].IsTrue;
+var baseDirPath = arguments["-B"].ToString();
 
-foreach (var s in arguments["<pysource>"].AsList)
+
+List<string> pySourceArgs = new();
+foreach (var pySource in arguments["<pysource>"].AsList)
 {
-    var pySourcePath = s.ToString();
+    var pySourceArg = pySource.ToString();
+    if (String.IsNullOrEmpty(pySourceArg)) continue;
+    pySourceArgs.Add(pySourceArg);
+}
 
-    // Read the file
-    var text = System.IO.File.ReadAllText(pySourcePath, System.Text.Encoding.UTF8);
-    var pyCode = SourceText.From(text);
 
-    string csFileName, csCode;
-    GeneratorError[] errors;
+if (useStdout)
+{
+    string csFileNameWithoutExtension;
+    string csSource;
+    GeneratorError[]? errors = null;
 
-    if (Generator.Generate(
-            @namespace,
-            pySourcePath,
-            pyCode,
-            out csFileName,
-            out csCode,
-            out errors) == false)
+    if (Generator.CSharpStringFromPyFile(@namespace, pySourceArgs[0], Encoding.UTF8, out csFileNameWithoutExtension, out csSource, out errors) == false)
     {
-        continue;
+        // error
+        return 1;
     }
 
-    if (useStdout)
+    Console.WriteLine(csSource);
+    return 0; // we only process one file if writing to stdout
+}
+
+foreach (var pySourceArg in pySourceArgs)
+{
+    if (recursive && Directory.Exists(pySourceArg))
     {
-        Console.WriteLine(csCode);
-        return;
+        throw new NotImplementedException();
+    }
+    else if (File.Exists(pySourceArg))
+    {
+        throw new NotImplementedException();
     }
     else
     {
-        var path = Path.Join(Path.GetDirectoryName(pySourcePath), csFileName);
-        System.IO.File.WriteAllText(path, csCode, Encoding.UTF8);
-        Console.WriteLine($"Generated {path}");
+        // error
+        return 1;
     }
 }
 
-/*
-            var path = Path.Join(Path.GetDirectoryName(arg), $"{pascalFileName}.py.cs");
-            System.IO.File.WriteAllText(path, source, Encoding.UTF8);
-            Console.WriteLine($"Generated {path}");
-*/
+return 0;
