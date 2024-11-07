e83540fd3bb23d2178ea80c2e8ea673224f2ded7
CaptainWelpe <michael.keller@minesworld.de>
2024-11-07 08:55:18 +0100

    - updated docs with files from main branch - renamed IsInstance to IsType file

---
+++ src/CSnakes.Runtime/Python/IsType.cs
@@ -0,0 +1,12 @@
+﻿using CSnakes.Runtime.CPython;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSnakes.Runtime.Python;
+public class IsType
+{
+    public static bool IsBytes(PythonObject p) => CAPI.IsInstance(p, CAPI.PtrToPyBytesType);
+}
